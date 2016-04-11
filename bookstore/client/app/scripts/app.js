b6ca4645478c4d2bffece65b9e766e0d3b5c9343
Danjelous <daniel.brunner@live.at>
2016-04-11 10:45:53 +0200

    Sexy Login

--- bookstore/client/app/scripts/app.js
+++ bookstore/client/app/scripts/app.js
@@ -1,6 +1,6 @@
 // Dependency Injection - Der Bookstore App zusätzlich das "ngRoute"-Modul injecten
 //   ("bookstore", ["ngRoute"]);    -->   vgl. PHP: include
-var bookstoreApp = angular.module("bookstore", ["ngRoute"]);
+var bookstoreApp = angular.module("bookstore", ["ngRoute", "ngCookies"]);
 
 
 // Eigener Filter
@@ -71,3 +71,60 @@ bookstoreApp.config(function($routeProvider){
 		redirectTo: "/books"
 	});
 });
+
+// "init"-method - gets called EVERYTIME something gets loaded
+// for example this runs evertime a browser refresh is executed
+bookstoreApp.run(function($rootScope, $location, $cookieStore, $http){
+
+	// Check the globals object for already existent cookies
+	$rootScope.globals = $cookieStore.get("globals") || {};
+
+	// Do i have a session already?
+	if ($rootScope.globals.currentUser) {
+
+		// Add our own object to the default HTTP-Header
+		$http.defaults.headers.common["Authorization"] =
+			$rootScope.globals.currentUser.authdata;
+	}
+
+	// If for example a user calls a restricted page (/admin/books eg.)
+ 	$rootScope.$on("$locationChangeStart", function(event, next, current){
+
+		// If someone has called a restrictedPage
+		var restrictedPage = $location.path().indexOf("/admin") != -1;
+
+		// is the user logged in?
+		var loggedIn = $rootScope.globals.currentUser;
+
+		if(restrictedPage && !loggedIn) {
+			// Bad guy isn't logged in but wants access to /admin
+			$location.path("/books");
+		}
+	});
+
+});
+
+// Interceptor Service
+bookstoreApp.factory("HttpErrorInterceptorService", ["$q", "$rootScope", "$location","FlashService",
+   function($q, $rootScope, $location,FlashService) {
+       var success = function(response) {
+               // pass through
+               return response;
+           },
+           error = function(response) {
+               if(response.status === 401) {
+                   FlashService.Error(response.data.message);
+               }
+
+               return $q.reject(response);
+           };
+
+       return function(httpPromise) {
+           return httpPromise.then(success, error);
+       };
+   }
+]).config(["$httpProvider",
+   function($httpProvider) {
+       $httpProvider.responseInterceptors.push("HttpErrorInterceptorService");
+   }
+]);
--- bookstore/client/app/scripts/controllers/Login.js
+++ bookstore/client/app/scripts/controllers/Login.js
@@ -1,7 +1,33 @@
 "use strict";
 
-// Insert BookDataService (Dependency Injection)
-bookstoreApp.controller("LoginCtrl", function($scope, BookDataService){
-	
+bookstoreApp.controller('LoginCtrl', function ($window,$scope,$location, $route, AuthenticationService,FlashService) {
 
+   $scope.logout = function () {
+        // reset login status
+       //AuthenticationService.ClearCredentials();
+       AuthenticationService.Logout();
+       $location.path('/books');
+       //$window.location.reload();
+    };
+
+    $scope.loggedIn = $scope.globals.currentUser;
+    console.log($scope.loggedIn);
+
+
+    $scope.login = function login(){
+        console.log("in login");
+        $scope.dataLoading = true;
+        AuthenticationService.Login($scope.username, $scope.password, function (response) {
+            if (response.success) {
+                //AuthenticationService.SetCredentials($scope.username, $scope.password);
+                $scope.loggedIn = true;
+                $location.path('/admin/books');
+                //$window.location.reload();
+            } else {
+                FlashService.Error(response.message);
+                $scope.dataLoading = false;
+                $scope.loggedIn = false;
+            }
+        });
+    };
 });
